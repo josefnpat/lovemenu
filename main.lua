90fdbe9dcbb2e11bbe6427822634a86f7249242a
Seppi <josefnpat@hotmail.com>
2011-11-19 02:51:24 -0500

    Wow, now it's much cooler. Eat it, alex!

--- main.lua
+++ main.lua
@@ -1,40 +1,60 @@
 require("menu/menu")
 function love.load()
   menu:toggle()
-  local view = {}
+  view = {}
   view[1] = {
-    title="Welcome to the game!",
+    title="Space Hell",
+    desc="Never before has mankind seen such a sexy menu screen. Presenting the lovemenu.",
     {t="New Game",cb="ng"},
     {t="Options",cb="op"},
+    {t="Credits",cb="cr"},
     {t="Exit",cb="exit"}
   }
   view[2] = {
-    title="Set your options here.",
+    title="Options",
+    desc="Set your options here.",
     {t="Fullscreen",cb="fs"},
-    {t="Change Res",cb="res"},
-    {t="Sound on/off",cb="sound"},
-    {t="Back",cb="mm"}
+    {t="Resolution ("..love.graphics.getWidth().."x"..love.graphics.getHeight()..")",cb="res"},
+    {t="Sound (on)",cb="sound"},
+    {t="Return",cb="mm"}
   }
   view[3] = {
-    title="Are you sure you want to exit?",
-    {t="Confirm Quit",cb="cexit"},
-    {t="Return to Main Menu",cb="mm"}
+    title="Quit",
+    desc="Are you sure you want to quit?",
+    {t="Confirm",cb="cexit"},
+    {t="Return",cb="mm"}
   }
+  view[4] = {
+    title="Credits",
+    desc="Josef N Patoprsty - Programmer\nSomeone Else - Artist",
+    {t="Return",cb="mm"}
+  }
+  videomodes = love.graphics.getModes()
+  currentmode = 1
   menu:load(view)
 end
 function love.draw()
   menu:draw()
+  if not menu.run then
+    love.graphics.print("Press escape to return to the menu!",128,128)
+  end
 end
 
 function love.keypressed(key)
   menu:keypressed(key)
+  if not menu.run and key == "escape" then
+    menu:toggle()
+  end
 end
 
+sound = true
 function menu:callback(cb)
   if cb == "ng" then
     menu:toggle()
   elseif cb == "op" then
     menu:setstate(2)
+  elseif cb == "cr" then
+    menu:setstate(4)
   elseif cb == "exit" then
     menu:setstate(3)
   elseif cb == "cexit" then
@@ -42,10 +62,29 @@ function menu:callback(cb)
   elseif cb == "fs" then
     love.graphics.toggleFullscreen( )
   elseif cb == "res" then
-    print("change res")
+    love.graphics.setMode( videomodes[currentmode].width, videomodes[currentmode].height )
+    view[2][2].t = "Resolution ("..love.graphics.getWidth().."x"..love.graphics.getHeight()..")"
+    currentmode = ((currentmode + 1)% #videomodes)+1
   elseif cb == "sound" then
-    print("muted!")
+    sound = not sound
+    local temp_x = ""
+    if sound then
+      temp_s = "on"
+    else
+      temp_s = "off"
+    end
+    view[2][3].t = "Sound ("..temp_s..")"
   elseif cb == "mm" then
     menu:setstate(1)
+  else
+    print("unknown command:"..cb)
   end
+end
+
+function love.update(dt)
+  menu:update(dt)
+end
+
+function love.mousepressed(x,y,button)
+  menu:mousepressed(x,y,button)
 end
